47e8288de56cc8c6eea621fc834d6d0615b64975
Icaxus <yunusssarica@gmail.com>
2023-12-11 00:44:00 +0300

    new upgrades about dice and scripts with fbx of dices

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,20 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
-public class Bouncable : MonoBehaviour
+public class Dice : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _force = 10.0f;
+    [SerializeField] private List<GameObject> edges;
+    protected bool grounded;
+    protected int diceUp;
+    [SerializeField] private GameObject _tmpDice;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         transform.rotation = Random.rotation;
+        
     }
 
     // Update is called once per frame
@@ -23,11 +31,21 @@ public class Bouncable : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             
-            Debug.Log("Space pressed");
+            //Debug.Log("Space pressed");
             //_rigidbody.AddExplosionForce(5f, transform.position, 5.0f, 3.0f);
             _rigidbody.AddForce(Vector3.up * _force * 2, ForceMode.Impulse);
             _rigidbody.AddTorque(Random.rotation.eulerAngles * _force);
             
+        }
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            grounded = false;
         }
     }
 
@@ -35,7 +53,7 @@ public class Bouncable : MonoBehaviour
     {
         if (other.CompareTag("Ground"))
         {
-            Debug.Log("Ground triggered!");
+            grounded = true;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private List<Dice> dices;
     void Start()
     {
         
@@ -12,6 +13,22 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            //Debug.Log("Space pressed");
+            //_rigidbody.AddExplosionForce(5f, transform.position, 5.0f, 3.0f);
+            foreach (var dice in dices)
+            {
+                // dice._rigidbody.AddForce(Vector3.up * _force * 2, ForceMode.Impulse);
+                // dice._rigidbody.AddTorque(Random.rotation.eulerAngles * _force);
+            }
+            
+        }
+    }
+
+    void GetDiceValues()
     {
         
     }
